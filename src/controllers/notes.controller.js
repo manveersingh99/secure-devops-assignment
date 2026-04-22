f5be4e840532ccb6e03759032beb99dbc573f9a1
Ubuntu <ubuntu@ip-172-31-29-205.ec2.internal>
2026-04-22 10:52:13 +0000

    Task 1 submission

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -7,16 +7,21 @@ ntsCtrl.renderNoteForm = (req, res) => {
 
 ntsCtrl.renderAddNote = async (req, res) => {
     const { noteDescription, noteTitle } = req.body;
-    const newNote = new Note({ title: noteTitle, description: noteDescription, user: req.user._id });
+    const newNote = new Note({
+        title: noteTitle,
+        description: noteDescription,
+        user: req.user._id
+    });
     await newNote.save();
     req.flash('success_msg', 'Note added');
     res.redirect('/notes');
 };
 
 ntsCtrl.renderAllNotes = async (req, res) => {
-    const note = await Note.find({ user: req.user._id }).sort({ createdAt: 'desc' });
-    res.render('notes/all-notes', { note });
-}
+    const notes = await Note.find().lean().sort({ createdAt: 'desc' });
+    console.log("NOTES DATA:", notes); // debug
+    res.render('notes/all-notes', { notes });
+};
 
 ntsCtrl.renderEditNote = async (req, res) => {
     const note = await Note.findById(req.params.id);
@@ -24,7 +29,7 @@ ntsCtrl.renderEditNote = async (req, res) => {
         req.flash('err_msg', 'What are you trying to do?');
         return res.redirect('/notes');
     }
-    res.render('notes/edit-note', { note })
+    res.render('notes/edit-note', { note });
 };
 
 ntsCtrl.renderUpdateNote = async (req, res) => {
@@ -34,10 +39,13 @@ ntsCtrl.renderUpdateNote = async (req, res) => {
         return res.redirect('/notes');
     }
     const { noteTitle, noteDescription } = req.body;
-    await Note.findByIdAndUpdate(req.params.id, { title: noteTitle, description: noteDescription })
+    await Note.findByIdAndUpdate(req.params.id, {
+        title: noteTitle,
+        description: noteDescription
+    });
     req.flash('success_msg', 'Note edited');
     res.redirect('/notes');
-}
+};
 
 ntsCtrl.renderDeleteNote = async (req, res) => {
     const note = await Note.findById(req.params.id);
@@ -48,6 +56,6 @@ ntsCtrl.renderDeleteNote = async (req, res) => {
     await Note.findByIdAndDelete(req.params.id);
     req.flash('success_msg', 'Note deleted');
     res.redirect('back');
-}
+};
 
 module.exports = ntsCtrl;
--- src/database.js
+++ src/database.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
-const {WEB_APP_NOTES_MONGODB_HOST,WEB_APP_NOTES_MONGODB_DATABASE} = process.env;
-const MONGODB_CONNECTION = `mongodb://${WEB_APP_NOTES_MONGODB_HOST}/${WEB_APP_NOTES_MONGODB_DATABASE}`;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/notesapp";
 
-mongoose.connect(MONGODB_CONNECTION, {
-    useUnifiedTopology: true,
-    useNewUrlParser:true,
-    useCreateIndex: true
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-    .then(db => console.log('DB Connection: true'))
-    .catch(err => console.log(err))
+.then(() => console.log('DB Connection: true'))
+.catch(err => console.log(err));
+
